9f4439d2d6d0ac4c6fde1b05b823855e51ace1b6
VIKTOR KHEGAI <vik51790660@gmail.com>
2026-06-06 20:01:04 +0900

    feat:add swiper

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ if (button.textContent === 'Показать все') {
 }
 });
 
+let swiper;
+
+function initSwiper() {
+  if (window.innerWidth <= 320 && !swiper) {
+    swiper = new Swiper('.swiper', {
+      slidesPerView: 'auto',
+      spaceBetween: 8,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+
+  if (window.innerWidth > 320 && swiper) {
+    swiper.destroy(true, true);
+    swiper = undefined;
+  }
+}
+
+initSwiper();
+
+window.addEventListener('resize', initSwiper);
